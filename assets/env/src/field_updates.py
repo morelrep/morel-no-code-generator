7713dcd25776accbc4b365608a924e95f3356641
Eduardo Febres <eduardofebresm@gmail.com>
2025-12-19 12:57:05 -0500

    call number y archive location added to the field updates script

--- assets/env/src/field_updates.py
+++ assets/env/src/field_updates.py
@@ -4,5 +4,7 @@ with open('assets/data/books_zotero.csv','r') as file:
     filedata = filedata.replace('File Attachments','Cover')
     filedata = filedata.replace('Url','Download')
     filedata = filedata.replace('Library Catalog','Library')
+    filedata = filedata.replace('Call Number','Call_Number')
+    filedata = filedata.replace('Archive Location','Archive_Location')
 with open('assets/data/books_zotero.csv','w') as file:
     file.write(filedata)
